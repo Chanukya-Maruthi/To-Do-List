523ac3d3abde40fa9bfa795c238dbb21d59b5ad6
Chanukya Maruthi Suravarapu <chanukyamaruthi75@gmail.com>
2023-09-30 23:19:46 +0530

    updated validations

--- client/src/Login.js
+++ client/src/Login.js
@@ -5,8 +5,58 @@ function Login({ onLogin }) {
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
+  // const handleLogin = async () => {
+  //   try {
+  //     const response = await fetch("/api/login", {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify({ email, password }),
+  //     });
+
+      
+
+  //     if (response.ok) {
+  //       const data = await response.json();
+  //       console.log(data)
+  //       const token = data.user; 
+  //       console.log(token['email'])
+  //       console.log(token['password'])
+  //       console.log(token["items"])
+  //       setError("");
+  //       onLogin(true,token["items"]);
+  //     } else {
+  //       setError("Invalid email or password.");
+  //     }
+  //   } catch (error) {
+  //     setError("An error occurred during login.");
+  //   }
+  // };
+
+  
+
   const handleLogin = async () => {
     try {
+      // Client-side validation
+      if (!email || !password) {
+        setError("Please provide both email and password.");
+        return;
+      }
+  
+      // Email validation using a regular expression
+      const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+      if (!emailPattern.test(email)) {
+        setError("Invalid email address.");
+        return;
+      }
+  
+      // Password validation (you can customize this as per your requirements)
+      if (password.length < 8) {
+        setError("Password must be at least 8 characters long.");
+        return;
+      }
+  
       const response = await fetch("/api/login", {
         method: "POST",
         headers: {
@@ -14,18 +64,12 @@ function Login({ onLogin }) {
         },
         body: JSON.stringify({ email, password }),
       });
-
-      
-
+  
       if (response.ok) {
         const data = await response.json();
-        console.log(data)
-        const token = data.user; 
-        console.log(token['email'])
-        console.log(token['password'])
-        console.log(token["items"])
+        const token = data.user;
         setError("");
-        onLogin(true,token["items"]);
+        onLogin(true, token.items);
       } else {
         setError("Invalid email or password.");
       }
@@ -33,7 +77,7 @@ function Login({ onLogin }) {
       setError("An error occurred during login.");
     }
   };
-
+  
   return (
     <div>
       <h1>Login</h1>
--- client/src/SignUp.js
+++ client/src/SignUp.js
@@ -5,8 +5,52 @@ function SignUp() {
   const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
 
+  // const handleSignUp = async () => {
+  //   try {
+  //     const response = await fetch("/api/signup", {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify({ email, password }),
+  //     });
+
+  //     console.log(response)
+
+  //     if (response.ok) {
+  //       setMessage("Registration successful");
+  //     } else {
+  //       const data = await response.json();
+  //       setMessage(data.message || "Registration failed");
+  //     }
+  //   } catch (error) {
+  //     setMessage("An error occurred during registration.");
+  //   }
+  // };
+  
+
+
   const handleSignUp = async () => {
     try {
+      // Client-side validation
+      if (!email || !password) {
+        setMessage("Please provide both email and password.");
+        return;
+      }
+  
+      // Email validation using a regular expression
+      const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+      if (!emailPattern.test(email)) {
+        setMessage("Invalid email address.");
+        return;
+      }
+  
+      // Password validation (you can customize this as per your requirements)
+      if (password.length < 8) {
+        setMessage("Password must be at least 8 characters long.");
+        return;
+      }
+  
       const response = await fetch("/api/signup", {
         method: "POST",
         headers: {
@@ -14,19 +58,18 @@ function SignUp() {
         },
         body: JSON.stringify({ email, password }),
       });
-
-      console.log(response)
-
+  
       if (response.ok) {
-        setMessage("Registration successful");
+        setMessage("Registration successful.");
       } else {
         const data = await response.json();
-        setMessage(data.message || "Registration failed");
+        setMessage(data.message || "Registration failed.");
       }
     } catch (error) {
       setMessage("An error occurred during registration.");
     }
   };
+  
 
   return (
     <div>
